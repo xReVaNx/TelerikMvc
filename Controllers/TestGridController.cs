c284c8d8bf621546c360f281f957dfa6f86a9b38
xReVaNx <kamil.p.rutyna@gmail.com>
2021-05-21 10:40:03 +0200

    Initial commit

---
+++ Controllers/TestGridController.cs
@@ -0,0 +1,30 @@
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TelerikMvc.Models;
+
+namespace TelerikMvc.Controllers
+{
+    public class TestGridController : Controller
+    {
+        // GET: TestGrid
+        [HttpGet]
+        public ActionResult Grid_Read([DataSourceRequest]DataSourceRequest request)
+        {
+            Person[] personList = new Person[]
+            {
+                new Person {Firstname = "Adam", Lastname = "Nowak", Company = "CDP", ExperienceInYears = 10},
+                new Person {Firstname = "Kamil", Lastname = "Kowalski", Company = "Accenture", ExperienceInYears = 5},
+                new Person {Firstname = "Dariusz", Lastname = "Kamiński", Company = "Microsoft", ExperienceInYears = 8},
+                new Person {Firstname = "Maciej", Lastname = "Dołęgowski", Company = "Google", ExperienceInYears = 2},
+                new Person {Firstname = "Dominik", Lastname = "Stachurski", Company = "IBM", ExperienceInYears = 3}
+            };
+
+            return Json(personList.ToDataSourceResult(request), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TelerikMvc.Models
+{
+    public class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Company { get; set; }
+        public int ExperienceInYears { get; set; }
+    }
+}
